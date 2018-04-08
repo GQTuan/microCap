0bcc26c0ac755c9ec50f35a2e3b4322fcdbf3262
zhangguibao <13484938079@163.com>
2018-04-08 10:17:20 +0800

    xxxx

--- frontend/views/layouts/_footer.php
+++ frontend/views/layouts/_footer.php
@@ -3,25 +3,21 @@
     <ul class="flex-nowrap">
         <li class="active">
             <a href="<?= url(['site/shop']) ?>">
-                <i class="iconfont icon-shouye"></i>
                 <p>首页</p>
             </a>
         </li>
         <li>
             <a href="<?= url(['order/position']) ?>">
-                <i class="iconfont icon-icon04"></i>
                 <p>订单</p>
             </a>
         </li>
         <li>
             <a href="<?= url(['site/cart']) ?>">
-                <i class="iconfont icon-gouwuche"></i>
                 <p>购物车</p>
             </a>
         </li>
         <li>
             <a href="<?= url(['site/my']) ?>">
-                <i class="iconfont icon-yonghuming"></i>
                 <p>我的</p>
             </a>
         </li>
--- frontend/views/site/shop.php
+++ frontend/views/site/shop.php
@@ -203,34 +203,30 @@
             </div>
             
     <!--footer star-->
-    <footer class="page-footer fixed-footer" id="footer">
+    <!-- <footer class="page-footer fixed-footer" id="footer">
         <ul>
             <li class="active">
                 <a href="<?= url(['site/shop']) ?>">
-                    <i class="iconfont icon-shouye"></i>
                     <p>首页</p>
                 </a>
             </li>
             <li>
                 <a href="<?= url(['order/position']) ?>">
-                    <i class="iconfont icon-icon04"></i>
                     <p>订单</p>
                 </a>
             </li>
             <li>
                 <a href="<?= url(['site/cart']) ?>">
-                    <i class="iconfont icon-gouwuche"></i>
                     <p>购物车</p>
                 </a>
             </li>
             <li>
                 <a href="<?= url(['site/my']) ?>">
-                    <i class="iconfont icon-yonghuming"></i>
                     <p>我的</p>
                 </a>
             </li>
         </ul>
-    </footer>
+    </footer> -->
     <!--footer end-->
 </body>
 <script>
