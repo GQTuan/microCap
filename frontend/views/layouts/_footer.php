a08ee1f6958e513b18b57f8ae3503acf6bcd212f
zhangguibao <13484938079@163.com>
2018-04-08 17:38:24 +0800

    xxx

--- frontend/views/layouts/_footer.php
+++ frontend/views/layouts/_footer.php
@@ -17,7 +17,7 @@
             </a>
         </li>
         <li>
-            <a href="<?= url(['site/my']) ?>">
+            <a href="<?= url(['user/index']) ?>">
                 <p>我的</p>
             </a>
         </li>
