89ddd1cc24c68243dd016f74743fe9f34d140741
bruce <376394074@qq.com>
2018-04-10 18:53:10 +0800

    add pay

--- frontend/views/layouts/_footer.php
+++ frontend/views/layouts/_footer.php
@@ -1,16 +1,16 @@
 <footer class="page-footer fixed-footer" id="footer">
     <ul class="flex-nowrap">
-        <li class="active">
+        <li class="<?php if ($this->context->id == 'site') {echo 'active';} ?>">
             <a href="<?= url(['site/shop']) ?>">
                 <p>首页</p>
             </a>
         </li>
-        <li>
+        <li class="<?php if ($this->context->module->requestedRoute == 'order/trans-detail') {echo 'active';} ?>">
             <a href="<?= url(['order/position']) ?>">
                 <p>订单</p>
             </a>
         </li>
-        <li>
+        <li class="<?php if ($this->context->id == 'user') {echo 'active';} ?>">
             <a href="<?= url(['user/index']) ?>">
                 <p>我的</p>
             </a>
